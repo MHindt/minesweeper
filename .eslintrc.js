7d9e47f2f3aa0b2fde566b470b92484a8b4874c2
MHindt <75388368+MHindt@users.noreply.github.com>
2022-12-12 13:49:44 -0500

    adding logic to field

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,6 +9,6 @@ module.exports = {
     ecmaVersion: "latest",
     sourceType: "module",
   },
-  plugins: ["react", "@typescript-eslint","prettier"],
+  plugins: ["react", "@typescript-eslint", "prettier"],
   rules: {},
 };
--- Types/fields.ts
+++ Types/fields.ts
@@ -9,3 +9,29 @@ export const CellState: Record<string, Cell> = {
     mark: 11,
     weakMark: 12,
 }
+export const emptyFieldGenerator = (size: number, state: Cell = CellState.empty): Field =>  new Array(size).fill(null).map(() => new Array(size).fill(state));
+
+
+export const fieldGenerator = (size:number, density:number): Field => {
+    if(density > 1 || density < 0 ) {
+         throw new Error('Density must be between 0 and 1');
+    }
+    let unprocessedCells = size * size;
+    let cellsWithBomb = unprocessedCells * density;
+    const result: Field = emptyFieldGenerator(size);
+    for(let i = 0; i < size; i += 1) {
+        for(let j = 0; j < size; j++) {
+            //no bombs
+            if(cellsWithBomb === 0) {
+                return result;
+            }
+            //with bombs
+            if(cellsWithBomb > 0) {
+                result[i][j] = CellState.bomb;
+                cellsWithBomb =- 1;
+            }
+            unprocessedCells =-1;
+        }
+    }
+    return result;
+}
